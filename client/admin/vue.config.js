0145e7e50dfe91f8c2a68912c8458ffa113e63bf
Chizhik <yura@chizhik.net>
2019-07-25 15:48:25 +0300

    UPD: remove i18 && i18 loader

--- client/admin/vue.config.js
+++ client/admin/vue.config.js
@@ -7,12 +7,6 @@ module.exports = {
   //},
 
   pluginOptions: {
-    i18n:   {
-      locale:         'ru',
-      fallbackLocale: 'ru',
-      localeDir:      'locales',
-      enableInSFC:    true
-    },
     moment: {
       locales: ['ru']
     }
