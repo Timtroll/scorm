af3096dc74d141b63ff567eba536f5621a44a95d
Chizhik <yura@chizhik.net>
2019-08-02 23:49:22 +0300

    UPD: table action button position

--- client/admin/src/components/ui/table/Table.vue
+++ client/admin/src/components/ui/table/Table.vue
@@ -3,47 +3,44 @@
     <table class="uk-table pos-table uk-table-striped uk-table-hover uk-table-divider uk-table-small  uk-table-middle">
       <thead class="pos-table-header">
       <tr>
-        <!--checked all-->
-        <th class="uk-table-shrink pos-table-checkbox">
-          <input type="checkbox"
-                 class="pos-checkbox-switch xsmall"
-                 @click="checkedAll">
-        </th>
+
         <!--header-->
         <th v-for="item in header"
             v-text="item"></th>
-        <th class="uk-table-shrink uk-text-nowrap">
-          действия
+        <th class="uk-text-right pos-table-checkbox"
+            width="95">
+          <input type="checkbox"
+                 class="pos-checkbox-switch xsmall">
         </th>
       </tr>
       </thead>
       <tbody>
 
       <tr v-for="row in data">
-        <!--check current-->
-        <td class="pos-table-checkbox">
-          <input type="checkbox"
-                 class="pos-checkbox-switch xsmall">
-        </td>
+
         <!--data-->
         <td v-for="item in row"
             class="pos-table-row uk-text-nowrap"
             v-text="item.val"></td>
 
-        <td class="uk-text-right uk-text-nowrap">
-          <a class="uk-icon-link uk-margin-small-left"
+        <!--check current-->
+        <td class="pos-table-checkbox uk-text-right uk-text-nowrap">
+          <a class="uk-icon-link uk-margin-small-right"
              @click.prevent="edit(row)">
             <img src="/img/icons/icon__edit.svg"
                  width="16"
                  height="16"
                  uk-svg></a>
 
-          <a class="uk-icon-link uk-margin-small-left"
+          <a class="uk-icon-link uk-link-muted uk-margin-small-right"
              @click.prevent="remove(row)">
             <img height="16"
                  src="/img/icons/icon__trash.svg"
                  uk-svg
                  width="16"></a>
+          <input type="checkbox"
+                 class="pos-checkbox-switch xsmall">
+
         </td>
       </tr>
       </tbody>
