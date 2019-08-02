8cbc5744f2c9cda9a12005ed9a2441548f000c21
Chizhik <yura@chizhik.net>
2019-08-02 20:28:14 +0300

    ADD: Loader icon

---
+++ client/admin/src/components/ui/icons/Loader.vue
@@ -0,0 +1,316 @@
+<template>
+  <svg xmlns="http://www.w3.org/2000/svg"
+       :width="width"
+       :height="height"
+       viewBox="0 0 48 48">
+
+    <!--cabels-->
+    <g>
+      <circle cx="24"
+              cy="29"
+              r="2"
+              fill="none"
+              stroke="#95a1ab"
+              stroke-linecap="round"
+              stroke-miterlimit="10"
+              stroke-width="2"></circle>
+      <line x1="24"
+            y1="23"
+            x2="24"
+            y2="27"
+            fill="none"
+            stroke="#95a1ab"
+            stroke-linecap="round"
+            stroke-miterlimit="10"
+            stroke-width="2"></line>
+      <polyline points="26 29 33 29 38 32"
+                fill="none"
+                stroke="#95a1ab"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"></polyline>
+      <line x1="24"
+            y1="34"
+            x2="24"
+            y2="31"
+            fill="none"
+            stroke="#95a1ab"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"></line>
+      <polyline points="22 29 15 29 11 32"
+                fill="none"
+                stroke="#95a1ab"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"></polyline>
+    </g>
+
+    <!--servers-->
+    <g>
+      <rect x="6"
+            y="1"
+            width="36"
+            height="9"
+            rx="2"
+            fill="none"
+            stroke="#666e78"
+            stroke-miterlimit="10"
+            stroke-width="2"></rect>
+      <rect x="6"
+            y="14"
+            width="36"
+            height="9"
+            rx="2"
+            fill="none"
+            stroke="#666e78"
+            stroke-miterlimit="10"
+            stroke-width="2"></rect>
+      <line x1="10"
+            y1="10"
+            x2="10"
+            y2="14"
+            fill="none"
+            stroke="#666e78"
+            stroke-miterlimit="10"
+            stroke-width="2"></line>
+      <line x1="38"
+            y1="10"
+            x2="38"
+            y2="14"
+            fill="none"
+            stroke="#666e78"
+            stroke-miterlimit="10"
+            stroke-width="2"></line>
+      <line x1="38"
+            y1="17"
+            x2="39"
+            y2="17"
+            fill="none"
+            stroke="#666e78"
+            stroke-linecap="round"
+            stroke-miterlimit="10"
+            stroke-width="2"></line>
+      <line x1="38"
+            y1="20"
+            x2="39"
+            y2="20"
+            fill="none"
+            stroke="#666e78"
+            stroke-linecap="round"
+            stroke-miterlimit="10"
+            stroke-width="2"></line>
+      <line x1="34"
+            y1="20"
+            x2="35"
+            y2="20"
+            fill="none"
+            stroke="#666e78"
+            stroke-linecap="round"
+            stroke-miterlimit="10"
+            stroke-width="2"></line>
+      <line x1="34"
+            y1="17"
+            x2="35"
+            y2="17"
+            fill="none"
+            stroke="#666e78"
+            stroke-linecap="round"
+            stroke-miterlimit="10"
+            stroke-width="2"></line>
+      <line x1="30"
+            y1="17"
+            x2="31"
+            y2="17"
+            fill="none"
+            stroke="#666e78"
+            stroke-linecap="round"
+            stroke-miterlimit="10"
+            stroke-width="2"></line>
+      <line x1="30"
+            y1="20"
+            x2="31"
+            y2="20"
+            fill="none"
+            stroke="#666e78"
+            stroke-linecap="round"
+            stroke-miterlimit="10"
+            stroke-width="2"></line>
+      <line x1="38"
+            y1="4"
+            x2="39"
+            y2="4"
+            fill="none"
+            stroke="#666e78"
+            stroke-linecap="round"
+            stroke-miterlimit="10"
+            stroke-width="2"></line>
+      <line x1="38"
+            y1="7"
+            x2="39"
+            y2="7"
+            fill="none"
+            stroke="#666e78"
+            stroke-linecap="round"
+            stroke-miterlimit="10"
+            stroke-width="2"></line>
+      <line x1="34"
+            y1="7"
+            x2="35"
+            y2="7"
+            fill="none"
+            stroke="#666e78"
+            stroke-linecap="round"
+            stroke-miterlimit="10"
+            stroke-width="2"></line>
+      <line x1="34"
+            y1="4"
+            x2="35"
+            y2="4"
+            fill="none"
+            stroke="#666e78"
+            stroke-linecap="round"
+            stroke-miterlimit="10"
+            stroke-width="2"></line>
+      <line x1="30"
+            y1="4"
+            x2="31"
+            y2="4"
+            fill="none"
+            stroke="#666e78"
+            stroke-linecap="round"
+            stroke-miterlimit="10"
+            stroke-width="2"></line>
+      <line x1="30"
+            y1="7"
+            x2="31"
+            y2="7"
+            fill="none"
+            stroke="#666e78"
+            stroke-linecap="round"
+            stroke-miterlimit="10"
+            stroke-width="2"></line>
+    </g>
+
+    <!--led-->
+    <g>
+      <circle class="led_1"
+              cx="10.5"
+              cy="18.5"
+              r="2.5"></circle>
+      <circle class="led_2"
+              cx="16.5"
+              cy="18.5"
+              r="2.5"></circle>
+      <circle class="led_3"
+              cx="22.5"
+              cy="18.5"
+              r="2.5"></circle>
+      <circle class="led_4"
+              cx="10.5"
+              cy="5.5"
+              r="2.5"></circle>
+      <circle class="led_5"
+              cx="16.5"
+              cy="5.5"
+              r="2.5"></circle>
+      <circle class="led_6"
+              cx="22.5"
+              cy="5.5"
+              r="2.5"></circle>
+    </g>
+
+    <!--pc 1-->
+    <g class="comp_1">
+      <rect x="1"
+            y="42"
+            width="13"
+            height="3"
+            rx="1.5"
+            fill="none"
+            stroke-miterlimit="10"
+            stroke-width="2"></rect>
+      <path d="M5,35h5a2,2,0,0,1,2,2v5a0,0,0,0,1,0,0H3a0,0,0,0,1,0,0V37A2,2,0,0,1,5,35Z"
+            fill="none"
+            stroke-miterlimit="10"
+            stroke-width="2"></path>
+      <line x1="8"
+            y1="38"
+            x2="9"
+            y2="38"
+            fill="none"
+            stroke-linecap="round"
+            stroke-miterlimit="10"
+            stroke-width="2"></line>
+    </g>
+
+    <!--pc 2-->
+    <g class="comp_2">
+      <rect x="17.5"
+            y="44"
+            width="13"
+            height="3"
+            rx="1.5"
+            fill="none"
+            stroke-miterlimit="10"
+            stroke-width="2"></rect>
+      <path d="M21.5,37h5a2,2,0,0,1,2,2v5a0,0,0,0,1,0,0h-9a0,0,0,0,1,0,0V39A2,2,0,0,1,21.5,37Z"
+            fill="none"
+            stroke-miterlimit="10"
+            stroke-width="2"></path>
+      <line x1="24"
+            y1="40"
+            x2="25.5"
+            y2="40"
+            fill="none"
+            stroke-linecap="round"
+            stroke-miterlimit="10"
+            stroke-width="2"></line>
+    </g>
+
+    <!--pc 3-->
+    <g class="comp_3">
+      <rect x="34"
+            y="42"
+            width="13"
+            height="3"
+            rx="1.5"
+            fill="none"
+            stroke-miterlimit="10"
+            stroke-width="2"></rect>
+      <path d="M38,35h5a2,2,0,0,1,2,2v5a0,0,0,0,1,0,0H36a0,0,0,0,1,0,0V37A2,2,0,0,1,38,35Z"
+            fill="none"
+            stroke-miterlimit="10"
+            stroke-width="2"></path>
+      <line x1="41"
+            y1="38"
+            x2="42"
+            y2="38"
+            fill="none"
+            stroke-linecap="round"
+            stroke-miterlimit="10"
+            stroke-width="2"></line>
+    </g>
+  </svg>
+</template>
+
+<script>
+  export default {
+    name:  'Loader',
+    props: {
+      width:  {
+        default: 100,
+        type:    Number
+      },
+      height: {
+        default: 100,
+        type:    Number
+      }
+    }
+  }
+</script>
+
+<style lang="sass"
+       src="../../../assets/sass/incomponent/_loader.sass"
+       scoped>
+</style>
