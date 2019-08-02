45c5fc336cb5f268ae91a60076bde7564a1be58c
Chizhik <yura@chizhik.net>
2019-08-02 15:19:03 +0300

    ADD: table view - 30%

--- client/admin/src/components/settings/Settings.vue
+++ client/admin/src/components/settings/Settings.vue
@@ -7,7 +7,11 @@
         :footer-right="true"
         :body-left="true"
         :body-left-padding="false"
-        :body-right="true">
+        :body-left-toggle-show="true"
+        :body-right="true"
+        :body-right-toggle-show="false"
+        :body-right-show="card.bodyRightShow"
+        :body-padding="false">
 
     <!-- // header // -->
     <template #headerLeft>+</template>
@@ -16,22 +20,22 @@
       <div class="uk-grid-small"
            uk-grid>
         <div>
-          <a class="uk-button-default uk-button uk-button-small">
+          <button class="uk-button-default uk-button uk-button-small">
             <img src="/img/icons/icon__close.svg"
                  uk-svg
-                 width="12"
-                 height="12">
+                 width="10"
+                 height="10">
             <span class="uk-margin-small-left">отменить</span>
-          </a>
+          </button>
         </div>
         <div>
-          <a class="uk-button-success uk-button uk-button-small">
+          <button class="uk-button-success uk-button uk-button-small">
             <img src="/img/icons/icon__save.svg"
                  uk-svg
-                 width="16"
-                 height="16">
+                 width="14"
+                 height="14">
             <span class="uk-margin-small-left">сохранить</span>
-          </a>
+          </button>
         </div>
       </div>
     </template>
@@ -43,7 +47,10 @@
 
     <!-- // Body // -->
     <template #body>
-
+      <Table :header="table.header"
+             :data="table.data"
+             v-on:edit="editEl($event)"
+             :settings="table.settings"></Table>
     </template>
 
     <!--bodyLeft-->
@@ -52,7 +59,34 @@
     </template>
 
     <!--bodyRight-->
-    <template #bodyRight>bodyRight</template>
+    <template #bodyRight>
+      <List :data="card.bodyRightContent"
+            :labels="table.header"></List>
+    </template>
+
+    <template #bodyRightFooter>
+      <div class="uk-flex uk-flex-between uk-width-1-1">
+        <div class="">
+          <button class="uk-button-default uk-button uk-button-small"
+                  @click.prevent="card.bodyRightShow = false">
+            <img src="/img/icons/icon__close.svg"
+                 uk-svg
+                 width="10"
+                 height="10">
+            <span class="uk-margin-small-left">отменить</span>
+          </button>
+        </div>
+        <div class="">
+          <button class="uk-button-success uk-button uk-button-small">
+            <img src="/img/icons/icon__save.svg"
+                 uk-svg
+                 width="14"
+                 height="14">
+            <span class="uk-margin-small-left">сохранить</span>
+          </button>
+        </div>
+      </div>
+    </template>
 
   </Card>
 </template>
@@ -62,17 +96,66 @@
   import NavTree from '../ui/tree/NavTree'
   import Tree from '../ui/tree/Tree'
   import IconBug from '../ui/icons/IconBug'
+  import Table from '../ui/table/Table'
+  import List from '../ui/list/List'
 
   export default {
 
     name: 'Settings',
 
-    components: {IconBug, Tree, NavTree, Card},
+    components: {List, Table, IconBug, Tree, NavTree, Card},
 
     data () {
       return {
 
         bodyComponent: null,
+        card:          {
+          bodyRightShow:    false,
+          bodyRightContent: []
+        },
+
+        table: {
+          settings: {readOnly: false},
+          header:   ['id', 'alias', 'title', 'type', 'default_value', 'set'],
+          data:     [
+            [
+              {type: 'int', editable: false, validation: null, val: 1},
+              {type: 'string', editable: true, validation: null, val: 'id'},
+              {type: 'string', editable: true, validation: null, val: 'id'},
+              {type: 'select', editable: true, validation: null, val: 'int'},
+              {type: 'string', editable: true, validation: null, val: null},
+              {type: 'string', editable: true, validation: null, val: 'office'}
+            ], [
+              {type: 'int', editable: false, validation: null, val: 2},
+              {type: 'string', editable: true, validation: null, val: 'name'},
+              {type: 'string', editable: true, validation: null, val: 'Название организации'},
+              {type: 'select', editable: true, validation: null, val: 'string'},
+              {type: 'string', editable: true, validation: null, val: null},
+              {type: 'string', editable: true, validation: null, val: 'office'}
+            ], [
+              {type: 'int', editable: false, validation: null, val: 3},
+              {type: 'string', editable: true, validation: null, val: 'system_name'},
+              {type: 'string', editable: true, validation: null, val: 'Системное название'},
+              {type: 'select', editable: true, validation: null, val: 'string'},
+              {type: 'string', editable: true, validation: null, val: null},
+              {type: 'string', editable: true, validation: null, val: 'office'}
+            ], [
+              {type: 'int', editable: false, validation: null, val: 4},
+              {type: 'string', editable: true, validation: null, val: 'parent_id'},
+              {type: 'string', editable: true, validation: null, val: 'Родитель'},
+              {type: 'select', editable: true, validation: null, val: 'int'},
+              {type: 'string', editable: true, validation: null, val: null},
+              {type: 'string', editable: true, validation: null, val: 'office'}
+            ], [
+              {type: 'int', editable: false, validation: null, val: 5},
+              {type: 'string', editable: true, validation: null, val: 'is_active'},
+              {type: 'string', editable: true, validation: null, val: 'Статус'},
+              {type: 'select', editable: true, validation: null, val: 'int'},
+              {type: 'string', editable: true, validation: null, val: null},
+              {type: 'string', editable: true, validation: null, val: 'office'}
+            ]
+          ]
+        },
 
         nav: [
           {
@@ -187,6 +270,16 @@
           }
         ]
       }
+    },
+
+    methods: {
+
+      editEl (event) {
+        this.card.bodyRightContent = event
+        this.card.bodyRightShow    = !this.card.bodyRightShow
+      },
+
+      removeEl () {}
     }
 
   }
---
+++ client/admin/src/components/ui/table/Table.vue
@@ -0,0 +1,93 @@
+<template>
+  <div class="uk-overflow-auto uk-height-1-1 uk-position-relative">
+    <table class="uk-table pos-table uk-table-striped uk-table-hover uk-table-divider uk-table-small  uk-table-middle">
+      <thead class="pos-table-header">
+      <tr>
+        <!--checked all-->
+        <th class="uk-table-shrink pos-table-checkbox">
+          <input type="checkbox"
+                 class="pos-checkbox-switch xsmall"
+                 @click="checkedAll">
+        </th>
+        <!--header-->
+        <th v-for="item in header"
+            v-text="item"></th>
+        <th class="uk-table-shrink uk-text-nowrap">
+          действия
+        </th>
+      </tr>
+      </thead>
+      <tbody>
+
+      <tr v-for="row in data">
+        <!--check current-->
+        <td class="pos-table-checkbox">
+          <input type="checkbox"
+                 class="pos-checkbox-switch xsmall">
+        </td>
+        <!--data-->
+        <td v-for="item in row"
+            class="pos-table-row uk-text-nowrap"
+            v-text="item.val"></td>
+
+        <td class="uk-text-right uk-text-nowrap">
+          <a class="uk-icon-link uk-margin-small-left"
+             @click.prevent="edit(row)">
+            <img src="/img/icons/icon__edit.svg"
+                 width="16"
+                 height="16"
+                 uk-svg></a>
+
+          <a class="uk-icon-link uk-margin-small-left"
+             @click.prevent="remove(row)">
+            <img height="16"
+                 src="/img/icons/icon__trash.svg"
+                 uk-svg
+                 width="16"></a>
+        </td>
+      </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Table',
+
+    props: {
+
+      settings: {
+        type: Object
+      },
+      header:   {
+        type:     Array,
+        required: true
+      },
+
+      data: {
+        type: Array
+      }
+
+    },
+
+    data () {
+      return {}
+    },
+
+    methods: {
+
+      edit (event) {
+        this.$emit('edit', event)
+      },
+
+      remove (event) {
+        this.$emit('remove', event)
+      },
+
+      checkedAll () {
+        console.log('checkedAll')
+      }
+    }
+  }
+</script>
