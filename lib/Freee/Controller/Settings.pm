75ca3e836a833d7a7cdacf8ee3a708a578473445
Timofey Zhuravlev <timtroll@yandex.ru>
2019-08-03 11:46:21 +0300

    Add fucn to Hepler, add new routes

--- lib/Freee/Controller/Settings.pm
+++ lib/Freee/Controller/Settings.pm
@@ -45,19 +45,88 @@ sub index {
     );
 }
 
-sub update {
+sub set_tab_list {
     my ($self);
     $self = shift;
 
-    # сохраняем указанные настройки
-
-    $self->index();
+    $self->render(
+        'json'    => {
+            'controller'    => 'Settings',
+            'route'         => 'set_tab_list'
+        }
+    );
 }
 
-############# Subs #############
+sub set_addtab {
+    my ($self);
+    $self = shift;
 
-sub settings_list {
+    $self->render(
+        'json'    => {
+            'controller'    => 'Settings',
+            'route'         => 'set_addtab'
+        }
+    );
+}
 
+sub set_savetab {
+    my ($self);
+    $self = shift;
+
+    $self->render(
+        'json'    => {
+            'controller'    => 'Settings',
+            'route'         => 'set_savetab'
+        }
+    );
+}
+
+sub set_deletetab {
+    my ($self);
+    $self = shift;
+
+    $self->render(
+        'json'    => {
+            'controller'    => 'Settings',
+            'route'         => 'set_deletetab'
+        }
+    );
+}
+
+sub set_add {
+    my ($self);
+    $self = shift;
+
+    $self->render(
+        'json'    => {
+            'controller'    => 'Settings',
+            'route'         => 'set_add'
+        }
+    );
+}
+
+sub set_save {
+    my ($self);
+    $self = shift;
+
+    $self->render(
+        'json'    => {
+            'controller'    => 'Settings',
+            'route'         => 'set_save'
+        }
+    );
+}
+
+sub set_delete {
+    my ($self);
+    $self = shift;
+
+    $self->render(
+        'json'    => {
+            'controller'    => 'Settings',
+            'route'         => 'set_delete'
+        }
+    );
 }
 
 1;
