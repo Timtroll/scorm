bb43783a9b203e1543b97359d6e762b025e4377f
Timofey Zhuravlev <timtroll@yandex.ru>
2019-08-09 02:34:35 +0300

    Deploy

--- lib/Freee/Controller/Deploy.pm
+++ lib/Freee/Controller/Deploy.pm
@@ -2,14 +2,13 @@ package Freee::Controller::Deploy;
 
 use Mojo::Base 'Mojolicious::Controller';
 
-use Cwd qw(getcwd);
 use Data::Dumper;
 
 sub index {
     my ($self);
     $self = shift;
 
-    my $responce = `/usr/bin/flock -w 180 /var/tmp/deploy.lock /home/troll/workspace/scorm/deploy.sh &`;
+    my $responce = `/usr/bin/flock -w 180 /var/tmp/deploy.lock /home/troll/scorm/deploy.sh &`;
 
     $self->render(
         'json'    => 'ok'
