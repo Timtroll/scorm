7f62dde20ea1a1b1a3c8093d4801b568ccfa3e2a
Timofey Zhuravlev <timtroll@yandex.ru>
2019-08-09 02:20:46 +0300

    Deploy

--- deploy.sh
+++ deploy.sh
@@ -32,6 +32,7 @@ echo "-------"
 echo "yarn run build"
 cp -a /home/troll/scorm/client/admin/dist/. /home/troll/scorm/public
 
+
 # if [ "$?" != "0" ] ; then
 #     echo
 #     echo "ERROR: Execution failed."
