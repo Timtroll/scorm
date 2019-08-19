3b4b29013d27e19871599b4e8f5c30bfa510038d
Timofey Zhuravlev <timtroll@yandex.ru>
2019-08-19 19:56:06 +0300

    Fix Deploy route

--- lib/Freee/Controller/Deploy.pm
+++ lib/Freee/Controller/Deploy.pm
@@ -9,25 +9,14 @@ sub index {
     my ($self);
     $self = shift;
 
-    my ($status, $responce, $mess);
-
-    # if (-e "$FindBin::Bin/../log/deploy.flock") {
-    #     $status = 'fail';
-    #     $mess = 'Deploy working now';
-    # }
-    # else {
-        $responce = `/usr/bin/flock -x -w 180 $FindBin::Bin/../log/deploy.lock -c \"$FindBin::Bin/../deploy.sh\" > $FindBin::Bin/../log/deploy.log &`;
-print "/usr/bin/flock -x -w 180 $FindBin::Bin/../log/deploy.lock -c \"$FindBin::Bin/../deploy.sh\" > $FindBin::Bin/../log/deploy.log \&\n";
-        $status = 'ok';
-    # }
+    my ($status, $responce);
+    $responce = `/usr/bin/flock -x -w 180 $FindBin::Bin/../log/deploy.lock -c \"$FindBin::Bin/../deploy.sh\" > $FindBin::Bin/../log/deploy.log &`;
+    $status = 'ok';
 
     $self->render(
         'json' => {
             status      => $status,
             responce    => $responce,
-            mess        => $mess,
-            lock        => "$FindBin::Bin/../log/flock",
-            dir         => $FindBin::Bin
         }
     );
 }
