0376d60a2f3e704f94c3e432605ed5e81126c059
Timofey Zhuravlev <timtroll@yandex.ru>
2019-08-19 20:20:26 +0300

    Fix Deploy route

--- lib/Freee/Controller/Deploy.pm
+++ lib/Freee/Controller/Deploy.pm
@@ -9,17 +9,20 @@ sub index {
     my ($self);
     $self = shift;
 
-    my ($status, $responce, $lock);
+    my ($token, $status, $responce, $lock);
+    $token = $self->req->param('token');
 
+    # check lock file
     open(LOCK, "$FindBin::Bin/../log/deploy.flock") or $lock = 1;
     close (LOCK);
-    if ($lock) {
-        $status = 'fail';
-        $lock = 'Deploy working now';
-    }
-    else {
+
+    if (!$lock && ($self->config->{secrets}[0] eq $token)) {
         $responce = `/usr/bin/flock -x -w 120 $FindBin::Bin/../log/deploy.lock -c \"$FindBin::Bin/../deploy.sh\" > $FindBin::Bin/../log/deploy.log &`;
         $status = 'ok';
+    }
+    else {
+        $status = 'fail';
+        $lock = 'Deploy working now or token if fail';
     }
 
     $self->render(
