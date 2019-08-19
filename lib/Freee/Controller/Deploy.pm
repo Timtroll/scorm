f986d0e132bddc50ed05920df8db6868dfbe0e4c
Timofey Zhuravlev <timtroll@yandex.ru>
2019-08-19 19:38:17 +0300

    Fix Deploy route

--- lib/Freee/Controller/Deploy.pm
+++ lib/Freee/Controller/Deploy.pm
@@ -18,7 +18,7 @@ print "$root/log/flock\n========\n";
     }
     else {
         $responce = `/usr/bin/flock -x -w 180 $root/log/deploy.lock -c "$root/deploy.sh" > $root/deploy.log &`;
-print "/usr/bin/flock -x -w 180 $root/log/deploy.lock -c "$root/deploy.sh" > $root/deploy.log \&\n";
+print "/usr/bin/flock -x -w 180 $root/log/deploy.lock -c \"$root/deploy.sh\" > $root/deploy.log \&\n";
         $status = 'ok';
     }
 
