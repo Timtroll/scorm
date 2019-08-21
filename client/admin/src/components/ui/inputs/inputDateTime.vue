dc4406e7b40cce32fc6c6186c2502adf8cb944b6
yurgeman <yura@chizhik.net>
2019-08-21 22:50:19 +0300

    update inputeDateTime

---
+++ client/admin/src/components/ui/inputs/inputDateTime.vue
@@ -0,0 +1,139 @@
+<template>
+  <div class="uk-form-horizontal">
+    <div>
+      <label v-text="label || placeholder"
+             class="uk-form-label uk-text-truncate"
+             v-if="label || placeholder"></label>
+
+      <div class="uk-form-controls">
+        <div class="uk-inline uk-width-1-1">
+
+          <DatePick v-model="valueInput"
+                    :format="'YYYY-MM-DD HH:mm'"
+                    :displayFormat="'DD.MM.YYYY HH:mm'"
+                    :selectableYearRange="5"
+                    :mobileBreakpointWidth="480"
+                    :inputAttributes="{class: 'uk-input uk-width-1-1', readonly: true}"
+                    :pickTime="true"
+                    :pickMinutes="true"
+                    :months="$t('calendar.months')"
+                    :weekdays="$t('calendar.weekdays')"
+                    :setTimeCaption="$t('calendar.setTimeCaption')"
+                    :prevMonthCaption="$t('calendar.prevMonthCaption')"
+                    :nextMonthCaption="$t('calendar.nextMonthCaption')"
+                    @input="update">
+                    <!--:parseDate="parseDate"-->
+            <!--:formatDate="formatDate">-->
+          </DatePick>
+          <div class="uk-form-icon uk-form-icon-flip">
+            <img src="/img/icons/icon__input_calendar.svg"
+                 uk-svg
+                 width="18"
+                 height="18">
+
+          </div>
+
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+
+  import DatePick from 'vue-date-pick'
+  import 'vue-date-pick/dist/vueDatePick.css'
+
+  export default {
+    components: {DatePick},
+
+    name: 'inputDateTime',
+
+    props: {
+
+      value: {
+        default: null
+      },
+
+      label: {
+        default: null,
+        type:    String
+      },
+
+      placeholder: {
+        default: null,
+        type:    String
+      },
+
+      editable: {
+        default: true,
+        type:    Boolean
+      },
+
+      mask: {
+        type: String
+      },
+
+      required: {
+        default: false,
+        type:    Boolean
+      }
+    },
+
+    data () {
+      return {
+        valueInput: this.value,
+        valid:      true,
+
+        calendar: {
+          weekdays:         ['Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб', 'Вс'],
+          months:           ['Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь', 'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь'],
+          nextMonthCaption: 'След. месяц',
+          prevMonthCaption: 'Пред. месяц',
+          setTimeCaption:   'Установить время:'
+        }
+
+      }
+    },
+
+    computed: {
+
+      isChanged () {
+        return this.valueInput !== this.value
+      },
+
+      validate () {
+
+        let validClass = null
+        if (this.required) {
+          if (!this.valueInput && this.valueInput.length < 1) {
+            validClass = 'uk-form-danger'
+            this.valid = false
+            this.$emit('valid', this.valid)
+          } else {
+            validClass = 'uk-form-success'
+            this.valid = true
+            this.$emit('valid', this.valid)
+          }
+        }
+        return validClass
+      }
+    },
+
+    methods: {
+
+      parseDate (dateString, format) {
+        //return fecha.parse(dateString, format);
+      },
+      formatDate (dateObj, format) {
+        //return fecha.format(dateObj, format);
+      },
+
+      update () {
+        this.$emit('key')
+        this.$emit('change', this.isChanged)
+        this.$emit('value', this.valueInput)
+      }
+    }
+  }
+</script>
