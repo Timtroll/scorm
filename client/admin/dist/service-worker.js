ab2d56cf322e46b6b51aa7131db5d8a9622070ca
yurgeman <yura@chizhik.net>
2019-08-24 14:54:22 +0300

    BUILD

--- client/admin/dist/service-worker.js
+++ client/admin/dist/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d46c75972781d6854f5e0dee8a3dacca.js"
+  "/precache-manifest.39638e158a50ceb5f081bbce5b0a82ef.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "scorm_admin"});
