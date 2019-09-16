789eebf796a7eec90fa774ae9586faa58df6e01f
yurgeman <yura@chizhik.net>
2019-09-16 15:46:36 +0300

    Update Freee.pm

--- lib/Freee.pm
+++ lib/Freee.pm
@@ -61,26 +61,26 @@ print $self->beans_init(), "\n";
 
     my $auth = $r->under()                ->to('auth#check_token');
 
-    $auth->post('/settings/get_one')      ->to('settings#get_one');      # загрузка одной настройки
-    $auth->post('/settings/load_default') ->to('settings#load_default'); # загрузка дефолтных настроек
+    $auth->post('/settings/get_one')      ->to('settings#get_one');       # загрузка одной настройки
+    $auth->post('/settings/load_default') ->to('settings#load_default');  # загрузка дефолтных настроек
 
     # левая менюха (дерево)
-    $auth->post('/settings/proto_folder') ->to('settings#proto_folder'); # прототип для добавлениястроки (все поля)
+    $auth->post('/settings/proto_folder') ->to('settings#proto_folder');  # прототип для добавлениястроки (все поля)
     $auth->post('/settings/edit_tab')     ->to('settings#edit_tab');      # список полей для фолдера
-    $auth->post('/settings/get_list')     ->to('settings#get_list');     # список фолдеров/настроек по id парента (id 0 - корневые фолдеры, folder 0 - список детей)
+    $auth->post('/settings/get_list')     ->to('settings#get_list');      # список фолдеров/настроек по id парента (id 0 - корневые фолдеры, folder 0 - список детей)
 # ???????????? сделать полное дерево с текущим уровнем
-    $auth->post('/settings/get_tree')     ->to('settings#get_tree');     # дерево по id парента (id 0 - корневые фолдеры, folder 0 - список детей)
+    $auth->post('/settings/get_tree')     ->to('settings#get_tree');      # дерево по id парента (id 0 - корневые фолдеры, folder 0 - список детей)
     $auth->post('/settings/save_tab')     ->to('settings#save_tab');      # добавление/сохранение фолдера
     $auth->post('/settings/delete_tab')   ->to('settings#delete_tab');    # удаление фолдера
 
     # строки настроек
-    $auth->post('/settings/proto_row')    ->to('settings#proto_row');    # прототип для добавления строки (все поля)
-    $auth->post('/settings/edit')         ->to('settings#edit');         # добавление настройки
-    $auth->post('/settings/activate')     ->to('settings#activate');     # включение настройки
-    $auth->post('/settings/hide')         ->to('settings#hide');         # Список полей для редактирования настройки
-    $auth->post('/settings/save')         ->to('settings#save');         # добавление/сохранение настройки
+    $auth->post('/settings/proto_row')    ->to('settings#proto_row');     # прототип для добавления строки (все поля)
+    $auth->post('/settings/edit')         ->to('settings#edit');          # добавление настройки
+    $auth->post('/settings/activate')     ->to('settings#activate');      # включение настройки
+    $auth->post('/settings/hide')         ->to('settings#hide');          # Список полей для редактирования настройки
+    $auth->post('/settings/save')         ->to('settings#save');          # добавление/сохранение настройки
     # $auth->post('/settings/group_save')        ->to('settings#group_save');         # групповое добавление/сохранение настроек
-    $auth->post('/settings/delete')       ->to('settings#delete');       # удаление настройки
+    $auth->post('/settings/delete')       ->to('settings#delete');        # удаление настройки
 
     # управление контентом
     $auth->post('/cms/article')           ->to('cmsarticle#index');
