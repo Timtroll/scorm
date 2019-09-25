968aa64812ffd4ac471683d173b428d61a106aea
Yurgeman <Yurgeman@users.noreply.github.com>
2019-09-25 17:02:38 +0300

    Update actions.js

--- client/admin/src/store/modules/cms_table/actions.js
+++ client/admin/src/store/modules/cms_table/actions.js
@@ -1,13 +1,34 @@
 import router from './../../../router'
-import methods from './../../methods'
+import {flatTree, notify} from './../../methods'
 import Api from './../../../api/Table'
-//import UIkit from 'uikit/dist/js/uikit.min'
 
 const actions = {
 
-  getTable ({commit, state}, id) {
-    const table = require('../../../assets/mock/Table.json')
-    commit('set_table', table)
+  async getTable ({commit, state}, id) {
+    try {
+      commit('table_status_request')
+
+      const response = await Api.get_leafs(id)
+      //const routeId  = router.currentRoute.params.id
+
+      if (response.status === 200) {
+        const resp = await response.data
+
+        //if (typeof resp['list'] !== 'undefined') {
+        //  const table = resp.list
+        //
+        //}
+
+        commit('set_table', resp)
+        commit('table_status_success')
+
+      }
+
+    } catch (e) {
+      notify('ERROR: ' + e, 'danger')
+      throw 'ERROR: ' + e
+    }
+
   }
 
 }
