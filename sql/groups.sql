8d49c6598b067fad620a1b4fe881ebe2136dddbf
Timofey Zhuravlev <timtroll@yandex.ru>
2019-10-14 18:49:00 +0300

    Add removing group and it children

--- sql/groups.sql
+++ sql/groups.sql
@@ -1,6 +1,8 @@
+--таблица
 DROP TABLE IF EXISTS "public"."groups";
-
+DROP TRIGGER IF EXISTS "groups_ad" ON "public"."groups" CASCADE;
 DROP SEQUENCE IF EXISTS "public".groups_id_seq; 
+
 CREATE SEQUENCE "public".groups_id_seq;
 
 CREATE TABLE "public"."groups" (
@@ -14,3 +16,18 @@ WITH (OIDS=FALSE);
 ALTER TABLE "public"."groups" OWNER TO "troll";
 
 CREATE UNIQUE INDEX "groups_name_idx" ON "public"."groups" USING btree ("name");
+
+---функция (рекурсивное удаление детей)
+CREATE OR REPLACE FUNCTION "public"."groups_trigger_ad"() RETURNS "pg_catalog"."trigger" AS $BODY$
+BEGIN
+DELETE FROM "public"."routes" WHERE "parent" = OLD.id;
+
+RETURN OLD;
+END;
+$BODY$
+LANGUAGE 'plpgsql' VOLATILE COST 100;
+
+---триггер
+CREATE TRIGGER "groups_ad" AFTER DELETE ON "public"."groups"
+FOR EACH ROW
+EXECUTE PROCEDURE "groups_trigger_ad"();
