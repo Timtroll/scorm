587359aa5711e8a4a5ad7d6e3f757b710f1480cd
Yurgeman <Yurgeman@users.noreply.github.com>
2019-11-08 10:32:38 +0300

    id & parent (edit panel) => text

---
+++ client/admin/src/components/ui/inputs/InputInfo.vue
@@ -0,0 +1,51 @@
+<template>
+  <li>
+    <div class="uk-form-horizontal">
+      <div>
+        <label v-text="label"
+               class="uk-form-label uk-text-truncate"
+               v-if="label"/>
+
+        <div class="uk-form-controls">
+          <div class="uk-inline uk-width-1-1">
+            <div class="uk-form-icon uk-form-icon-flip">
+              <img src="/img/icons/icon__info.svg"
+                   uk-svg
+                   width="18"
+                   height="18">
+            </div>
+            <span class=""
+                  v-text="value"></span>
+
+          </div>
+        </div>
+      </div>
+    </div>
+  </li>
+</template>
+
+<script>
+  export default {
+    name: 'InputInfo',
+
+    props: {
+      value:       '',
+      name:        '',
+      label:       {
+        default: '',
+        type:    String
+      },
+      placeholder: {
+        default: '',
+        type:    String
+      },
+      readonly:    {default: 0, type: Number},
+      required:    {default: 0, type: Number},
+      mask:        {
+        type: RegExp
+      }
+
+    }
+
+  }
+</script>
