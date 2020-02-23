dd48a6d72eff6439a0d1aa225e931fc3adef6e9a
Timtroll <timtroll@yandex.ru>
2020-02-23 20:08:33 +0300

    Add mocks to User route

--- lib/Freee/Controller/User.pm
+++ lib/Freee/Controller/User.pm
@@ -70,113 +70,143 @@ sub index {
     $resp->{'data'} = $data if $data;
 
     $self->render( 'json' => $resp );
-
-    # $self->render(
-    #     'json'    => {
-    #         'controller'    => 'User',
-    #         'route'         => 'index',
-    #         'status'        => 'ok',
-    #         'params'        => $self->req->params->to_hash
-    #     }
-    # );
 }
 
 sub add {
     my ($self);
     $self = shift;
 
-    $self->render(
-        'json'    => {
-            'controller'    => 'User',
-            'route'         => 'add',
-            'status'        => 'ok',
-            'params'        => $self->req->params->to_hash
-        }
-    );
+    my ($data, $resp, @mess);
+    $data = {
+        'id'                => '',
+        'surname'           => '',  # Фамилия
+        'name'              => '',  # Имя
+        'patronymic'        => '',  # Отчество
+        'city'              => '',  # город
+        'country'           => '',  # страна
+        'timezone'          => '',  # часовой пояс
+        'birthday'          => 0,   # дата рождения (в секундах)
+        'email'             => '',  # email пользователя
+        'emailconfirmed'    => 0,   # email подтвержден
+        'phone'             => 0,   # номер телефона
+        'phoneconfirmed'    => 0,   # телефон подтвержден
+        'status'            => 0,   # активный / не активный пользователь
+        'groups'            => [],  # список ID групп
+        'password'          => '',  # хеш пароля
+        'avatar'            => ''
+    };
+
+    $resp->{'message'} = join("\n", @mess) if @mess;
+    $resp->{'status'} = @mess ? 'fail' : 'ok';
+    $resp->{'data'} = $data if $data;
+
+    $self->render( 'json' => $resp );
 }
 
 sub edit {
     my ($self);
     $self = shift;
 
-    $self->render(
-        'json'    => {
-            'controller'    => 'User',
-            'route'         => 'edit',
-            'status'        => 'ok',
-            'params'        => $self->req->params->to_hash
-        }
-    );
+
+    my ($data, $resp, @mess);
+    $data = {
+        'id'                => 1,
+        'surname'           => 'Фамилия',           # Фамилия
+        'name'              => 'Имя',               # Имя
+        'patronymic'        => 'Отчество',          # Отчество
+        'city'              => 'Санкт-Петербург',   # город
+        'country'           => 'Россия',            # страна
+        'timezone'          => '+3',                # часовой пояс
+        'birthday'          => 123132131,           # дата рождения (в секундах)
+        'email'             => 'username@ya.ru',    # email пользователя
+        'emailconfirmed'    => 1,                   # email подтвержден
+        'phone'             => 79312445646,         # номер телефона
+        'phoneconfirmed'    => 1,                   # телефон подтвержден
+        'status'            => 1,                   # активный / не активный пользователь
+        'groups'            => [1, 2, 3],           # список ID групп
+        'password'          => 'khasdf',            # хеш пароля
+        'avatar'            => 'https://thispersondoesnotexist.com/image'
+    };
+
+    $resp->{'message'} = join("\n", @mess) if @mess;
+    $resp->{'status'} = @mess ? 'fail' : 'ok';
+    $resp->{'data'} = $data if $data;
+
+    $self->render( 'json' => $resp );
 }
 
 sub save {
     my ($self);
     $self = shift;
 
-    $self->render(
-        'json'    => {
-            'controller'    => 'User',
-            'route'         => 'save',
-            'status'        => 'ok',
-            'params'        => $self->req->params->to_hash
-        }
-    );
+    my ($data, $resp, @mess);
+    $data = {
+        'id'                => 1,
+        'surname'           => 'Фамилия',           # Фамилия
+        'name'              => 'Имя',               # Имя
+        'patronymic'        => 'Отчество',          # Отчество
+        'city'              => 'Санкт-Петербург',   # город
+        'country'           => 'Россия',            # страна
+        'timezone'          => '+3',                # часовой пояс
+        'birthday'          => 123132131,           # дата рождения (в секундах)
+        'email'             => 'username@ya.ru',    # email пользователя
+        'emailconfirmed'    => 1,                   # email подтвержден
+        'phone'             => 79312445646,         # номер телефона
+        'phoneconfirmed'    => 1,                   # телефон подтвержден
+        'status'            => 1,                   # активный / не активный пользователь
+        'groups'            => [1, 2, 3],           # список ID групп
+        'password'          => 'khasdf',            # хеш пароля
+        'avatar'            => 'https://thispersondoesnotexist.com/image'
+    };
+
+    $resp->{'message'} = join("\n", @mess) if @mess;
+    $resp->{'status'} = @mess ? 'fail' : 'ok';
+    $resp->{'data'} = $data if $data;
+
+    $self->render( 'json' => $resp );
 }
 
-sub move {
-    my ($self);
-    $self = shift;
-
-    $self->render(
-        'json'    => {
-            'controller'    => 'User',
-            'route'         => 'move',
-            'status'        => 'ok',
-            'params'        => $self->req->params->to_hash
-        }
-    );
-}
 
 sub activate {
     my ($self);
     $self = shift;
 
-    $self->render(
-        'json'    => {
-            'controller'    => 'User',
-            'route'         => 'activate',
-            'status'        => 'ok',
-            'params'        => $self->req->params->to_hash
-        }
-    );
+    my ($data, $resp, @mess);
+    $data = {};
+
+    $resp->{'message'} = join("\n", @mess) if @mess;
+    $resp->{'status'} = @mess ? 'fail' : 'ok';
+    $resp->{'data'} = $data if $data;
+
+    $self->render( 'json' => $resp );
 }
 
 sub hide {
     my ($self);
     $self = shift;
 
-    $self->render(
-        'json'    => {
-            'controller'    => 'User',
-            'route'         => 'hide',
-            'status'        => 'ok',
-            'params'        => $self->req->params->to_hash
-        }
-    );
+    my ($data, $resp, @mess);
+    $data = {};
+
+    $resp->{'message'} = join("\n", @mess) if @mess;
+    $resp->{'status'} = @mess ? 'fail' : 'ok';
+    $resp->{'data'} = $data if $data;
+
+    $self->render( 'json' => $resp );
 }
 
 sub delete {
     my ($self);
     $self = shift;
 
-    $self->render(
-        'json'    => {
-            'controller'    => 'User',
-            'route'         => 'delete',
-            'status'        => 'ok',
-            'params'        => $self->req->params->to_hash
-        }
-    );
+    my ($data, $resp, @mess);
+    $data = {};
+
+    $resp->{'message'} = join("\n", @mess) if @mess;
+    $resp->{'status'} = @mess ? 'fail' : 'ok';
+    $resp->{'data'} = $data if $data;
+
+    $self->render( 'json' => $resp );
 }
 
 1;
