501a2e538b30566d4752095ca6a64498b35b09d2
AnosovSemen <Semeon222@yandex.ru>
2020-04-23 18:29:09 +0300

    checks and comments

--- script/migration.pl
+++ script/migration.pl
@@ -2,16 +2,25 @@
 
 use strict;
 use warnings;
-use File::Slurper 'read_text';
+use File::Slurp qw(slurp);
 use DBI;
+
+# миграция: чтение и запуск sql файлов из scorm/sql
 
 my $filename = '../freee.conf';
 my $dir = '../sql';
 
-my $config = read_text( $filename );
+# чтение конфигурации
+my $config = slurp( $filename, { err_mode => 'carp' } ) or do { 
+    logging( "can't read file $filename: $!" ); 
+    exit; 
+};
+
+# преобразование текста файла конфигурации в объект
 $config = eval( $config );
 my $db = $config->{'dbs'}->{'databases'}->{'pg_main'};
 
+# редактирование параметра обработки ошибок
 if (
     $db &&
     $db->{'options'} &&
@@ -20,27 +29,55 @@ if (
     $db->{'options'}->{'RaiseError'} = 0;
 }
 
+# подключение к базе данных
 my $dbh = DBI->connect(
     $db->{'dsn'},
     $db->{'username'},
     $db->{'password'},
     $db->{'options'}
 );
+if ( DBI->errstr ) {
+    logging( "connection doesn't work " . DBI->errstr . " in $filename script\n" );
+    exit;    
+}
 
-my @list = `ls $dir`;
+# чтение файлов директории скриптов
+my @list = `ls $dir 2>&1`;
+if ( $? ){
+    logging( "can't read directory $dir: @list" ); 
+    exit; 
+};
 
+# обработка файлов директории  
 foreach ( @list ){
     my $filename = $dir . '/' . $_;
     chomp ( $filename );
+
+    # фильтрация не sql файлов и папок
     next if ( -d $filename || $_ !~ /\.sql$/);
 
-    my $sql = read_text( $filename );
+    # чтение содержимого файлов
+    my $sql = slurp( $filename ) or do { 
+        logging( "can't read file $filename: $!" ); 
+        exit; 
+    };
+
+    # выполение скриптов
     my $sth = $dbh->prepare( $sql );
     my $res = $sth->execute();
-
-    unless ( $res ) {
-        my $error = DBI->errstr;
-        print "$error in $filename script!\n";
-        last;    
+    if ( DBI->errstr ) {
+        logging( "execute doesn't work " . DBI->errstr . " in $filename script\n" );
+        exit;    
     }
+}
+
+# логирование ошибки
+# logging( "комментарий и текст ошибки" );
+sub logging {
+    my $logdata = shift;
+    my $logname = '../log/migration.log';
+
+    open(my $log, '>>', $logname) or warn "Can't open log file!";
+    print $log "$logdata\n";
+    close $logname;
 }
