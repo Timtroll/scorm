188c6345fe1966802c5ffbe80bfc3beb60a65ee6
Timtroll <timtroll@yandex.ru>
2020-06-22 23:26:50 +0300

    Merge Refactoring Upload

--- sql/010-groups.sql
+++ sql/010-groups.sql
@@ -6,11 +6,11 @@ DROP SEQUENCE IF EXISTS "public".groups_id_seq;
 CREATE SEQUENCE "public".groups_id_seq;
 
 CREATE TABLE "public"."groups" (
-"id" int4 DEFAULT nextval('groups_id_seq'::regclass) NOT NULL,
-"label" varchar(255) COLLATE "default" NOT NULL,
-"name" varchar(255) COLLATE "default" NOT NULL,
-"status" int2 DEFAULT 1 NOT NULL,
-CONSTRAINT "groups_pkey" PRIMARY KEY ("id")
+    "id" int4 DEFAULT nextval('groups_id_seq'::regclass) NOT NULL,
+    "label" varchar(255) COLLATE "default" NOT NULL,
+    "name" varchar(255) COLLATE "default" NOT NULL,
+    "status" int2 DEFAULT 1 NOT NULL,
+    CONSTRAINT "groups_pkey" PRIMARY KEY ("id")
 )
 WITH (OIDS=FALSE);
 
