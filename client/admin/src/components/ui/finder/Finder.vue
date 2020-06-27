28d792e75aeb4b61ebbb321d573d0250e8c9ccf3
Yurgeman <Yurgeman@users.noreply.github.com>
2020-06-27 23:21:02 +0300

    start courses

---
+++ client/admin/src/components/ui/finder/Finder.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="pos-finder">
+
+    <FinderColumn :add="true"/>
+
+  </div>
+</template>
+
+<script>
+
+export default {
+  name:       'Finder',
+  components: {
+    FinderColumn: () => import(/* webpackChunkName: "FinderColumn" */ './FinderColumn')
+  },
+
+  props: {
+
+    data: {
+      type:    Object,
+      default: () => {}
+    }
+  },
+
+  data () {
+    return {}
+  },
+
+  methods: {}
+}
+</script>
+
+<style lang="sass">
+@import "sass/finder"
+</style>
