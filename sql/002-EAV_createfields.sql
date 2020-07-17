3c5a22579d0ca218141eb6d5768c8ebd8bc5b57a
Timtroll <timtroll@yandex.ru>
2020-07-17 18:24:30 +0300

    Add new fields for users table

--- sql/002-EAV_createfields.sql
+++ sql/002-EAV_createfields.sql
@@ -6,6 +6,7 @@ BEGIN
     PERFORM eav_createfield( 'User', 'patronymic', 'Отчество', 'string', NULL );
     PERFORM eav_createfield( 'User', 'name', 'Имя', 'string', NULL );
     PERFORM eav_createfield( 'User', 'surname', 'Фамилия', 'string', NULL );
+    PERFORM eav_createfield( 'Default', 'folder', 'Признак категории', 'boolean', NULL );
 END;
 $$;
 
@@ -19,8 +20,9 @@ INSERT INTO "public"."EAV_items" (
     date_created,
     title,
     parent,
-    has_childs
-) VALUES (TRUE, 0, 'User', NOW(), 'user_root', 0, 0);
+    has_childs,
+    folder
+) VALUES (TRUE, 0, 'User', NOW(), 'user_root', 0, 0, TRUE);
 
 INSERT INTO "public"."EAV_links" (
     parent,
