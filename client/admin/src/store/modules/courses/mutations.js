1c248b9b031785d20f1e0a836df7f1c7803451bf
Yurgeman <Yurgeman@users.noreply.github.com>
2020-08-04 17:01:30 +0300

    small fix

--- client/admin/src/store/modules/courses/mutations.js
+++ client/admin/src/store/modules/courses/mutations.js
@@ -4,19 +4,17 @@ const mutations = {
 
     data.list.forEach(i => {
       i.search = []
-      if (i.keywords) {
+
+      if (i.keywords)
         i.search.push(i.keywords)
-      }
-      if (i.description) {
+      if (i.description)
         i.search.push(i.description)
-      }
-      if (i.label) {
+      if (i.label)
         i.search.push(i.label)
-      }
+
       i.search = i.search.join(' ')
     })
     state.listRoot = data
-
   },
 
   setListLevel (state, data) {
