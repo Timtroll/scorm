a8d1e983db651c9b1ab2cfa665ed9aa35bcc52a4
Yurgeman <Yurgeman@users.noreply.github.com>
2020-08-11 20:16:33 +0300

    lesson - second video position

--- client/admin/src/layouts/lesson/Lesson.vue
+++ client/admin/src/layouts/lesson/Lesson.vue
@@ -1,10 +1,60 @@
 <template>
   <div class="pos-lesson-teach">
+
+    <!--VIDEO-->
     <div class="pos-lesson-teach-video">
-      <video width="1920" height="1080"></video>
+
+      <div class="pos-lesson-teach-video-outer">
+
+        <div class="pos-lesson-teach-video-screen main-screen">
+          <video width="1920"
+                 height="1080"
+                 autoplay
+                 playsinline
+                 muted
+                 loop
+                 controls
+                 preload="auto">
+            <source src="https://s3.eu-central-1.amazonaws.com/pipe.public.content/short.mp4">
+          </video>
+        </div>
+
+        <div class="pos-lesson-teach-video-screen second-screen"
+             :class="[secondScreen.position.v, secondScreen.position.h]">
+          <video width="1920"
+                 height="1080"
+                 v-touch:swipe="moveVideo"
+                 v-touch-options="{swipeTolerance: 10, touchHoldTolerance: 300}"
+                 autoplay
+                 playsinline
+                 muted
+                 loop
+                 preload="auto">
+            <source src="https://s3.eu-central-1.amazonaws.com/pipe.public.content/short.mp4">
+          </video>
+        </div>
+
+      </div>
+
+      <div class="pos-lesson-teach-video__controls"></div>
+
     </div>
-    <div class="pos-lesson-teach-users"></div>
-    <div class="pos-lesson-teach-content"></div>
+
+    <!--USERS-->
+    <div class="pos-lesson-teach-users">
+
+      <div class="pos-lesson-teach-users-header">
+        <div class="pos-lesson-teach-users-header__title"
+             v-text="$t('lesson.participants')"></div>
+      </div>
+
+      <div class="pos-lesson-teach-users-body">
+      </div>
+
+    </div>
+
+    <!--CONTENT-->
+    <div class="pos-lesson-teach-content">Content</div>
   </div>
 </template>
 
@@ -34,7 +84,37 @@ export default {
   },
 
   data () {
-    return {}
+    return {
+      secondScreen: {
+        position: {
+          v: 'right',
+          h: 'top'
+        }
+      }
+    }
+  },
+
+  methods: {
+
+    // moveVideo
+    moveVideo (direction) {
+      console.log(direction)
+      switch (direction) {
+        case ('left'):
+          this.secondScreen.position.v = 'left'
+          break
+        case ('right'):
+          this.secondScreen.position.v = 'right'
+          break
+        case ('top'):
+          this.secondScreen.position.h = 'top'
+          break
+        case ('bottom'):
+          this.secondScreen.position.h = 'bottom'
+          break
+      }
+
+    }
   }
 }
 </script>
