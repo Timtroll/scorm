fa00a367aa37565aa240fcd0e49b2c61e209f2a6
Yurgeman <Yurgeman@users.noreply.github.com>
2020-08-24 22:21:09 +0300

    Таблица: поля шапки = полям строки таблицы

--- client/admin/src/components/ui/cmsTable/Table.vue
+++ client/admin/src/components/ui/cmsTable/Table.vue
@@ -169,15 +169,15 @@
 
           <tbody>
           <TableRow
-                  :row-data="row"
-                  :mass-edit="massEdit"
-                  :editable="table.settings.editable"
-                  :removable="table.settings.removable"
-                  :full-data="filterSearch[index]"
-                  :checked-all="checked"
-                  v-for="(row, index) in tableRows"
-                  v-on:check="checkedAll"
-                  :key="index">
+            :row-data="row"
+            :mass-edit="massEdit"
+            :editable="table.settings.editable"
+            :removable="table.settings.removable"
+            :full-data="filterSearch[index]"
+            :checked-all="checked"
+            v-for="(row, index) in tableRows"
+            v-on:check="checkedAll"
+            :key="index">
           </TableRow>
           </tbody>
 
@@ -232,282 +232,293 @@
 
 <script>
 
-  import {clone} from '@/store/methods'
+import {clone} from '@/store/methods'
 
-  export default {
+export default {
 
-    name: 'Table',
+  name: 'Table',
 
-    components: {
-      TableRow: () => import(/* webpackChunkName: "TableRow" */ './TableRow'),
-      Card:     () => import(/* webpackChunkName: "Card" */ '../../ui/card/Card')
-    },
+  components: {
+    TableRow: () => import(/* webpackChunkName: "TableRow" */ './TableRow'),
+    Card:     () => import(/* webpackChunkName: "Card" */ '../../ui/card/Card')
+  },
 
-    data () {
-      return {
+  data () {
+    return {
 
-        //tableId: null,
+      //tableId: null,
 
-        searchInput: null,
-        checked:     false,
+      searchInput: null,
+      checked:     false,
 
-        card: {
-          add:    false,
-          folder: 0
-        },
-
-        header: [],
-
-        editRequired: {
-          name:        true,
-          placeholder: true,
-          label:       true,
-          type:        true
-        }
-
-      }
-    },
-
-    watch: {
-
-      table () {
-
-        if (this.table) {
-          const headerLocal = localStorage.getItem(this.pageComponent + '_header_' + this.tableId)
-
-          if (headerLocal) {
-            this.header = JSON.parse(headerLocal)
-          } else {
-            this.header = clone(this.protoLeaf)
-          }
-        }
-      }
-
-    },
-
-    async created () {
-
-      if (this.notEmptyTable === 'error') {
-        this.$store.commit('card_right_show', false)
-        this.$store.commit('tree_active', this.tableId)
-        await this.$store.dispatch(this.table_api.get, this.tableId)
-      }
-
-    },
-
-    async mounted () {
-
-      this.searchInput = null
-
-      //if (this.notEmptyTable === 'error') {
-      //  this.$store.commit('card_right_show', false)
-      //  this.$store.commit('tree_active', this.tableId)
-      //  await this.$store.dispatch(this.table_api.get, this.tableId)
-      //}
-
-    },
-
-    beforeRouteUpdate (to, from, next) {
-      // просто используйте `this`
-      this.searchInput = null
-      next()
-    },
-
-    beforeDestroy () {
-      this.$store.commit('editPanel_show', false)
-      this.$store.commit('tree_active', null)
-      this.$store.commit('set_editPanel_proto', [])
-      this.$store.commit('set_tree_proto', [])
-    },
-
-    computed: {
-
-      pageComponent () {return this.$route.name},
-
-      editPanel_api () { // список запросов для правой панели
-        return this.$store.getters.editPanel_api
+      card: {
+        add:    false,
+        folder: 0
       },
 
-      table_api () {return this.$store.getters.table_api},
+      header: [],
 
-      tableNotEmpty () {
-        if (this.table.body) {
-          return this.table.body.length > 0
-        }
-      },
-
-      tableEmptySearchResult () {
-        if (this.searchInput) {
-          return this.filterSearch.length === 0
-        }
-      },
-
-      tableId () {return this.$route.params.id},
-
-      loader () {return this.$store.getters.table_status},
-
-      protoLeaf () {return this.$store.getters.editPanel_proto},
-
-      notEmptyTable () {return (this.table && Object.keys(this.table).length === 0) ? 'error' : 'success'},
-
-      table () {return this.$store.getters.table_items},
-
-      allowAddChildren () {return this.$store.state.table.table.addChildren},
-
-      massEdit () {
-
-        if (this.table && this.table.settings && this.table.settings.massEdit) {
-          return this.table.settings.massEdit
-        } else {
-          return 0
-        }
-      },
-
-      removable () {
-
-        if (this.table && this.table.settings && this.table.settings.removable) {
-          return this.table.settings.removable
-        } else {
-          return 0
-        }
-      },
-
-      editable () {
-
-        if (this.table && this.table.settings && this.table.settings.editable) {
-          return this.table.settings.editable
-        } else {
-          return 0
-        }
-      },
-
-      tableRowDetail () {return this.$store.getters.pageTableRow},
-
-      // Шапка таблицы
-      tableHeader () {
-        if (this.header && this.header.length > 0) {
-          return this.header.filter(item => item.show === 1)
-        }
-      },
-
-      //async tableHeaderName () {
-      //  if (this.tableHeader && this.tableHeader.length > 0) {
-      //    return await this.tableHeader.map(item => item.name)
-      //  }
-      //},
-
-      //Тело таблицы
-      tableRows () {
-        if (this.filterSearch && this.tableHeader) {
-          const table        = this.filterSearch,
-                displayTable = [],
-                flatHeader   = this.tableHeader.map(item => item.name)
-
-          table.forEach((item) => {
-            const newItem = []
-
-            flatHeader.forEach((headItem, i) => {
-
-              if (item.hasOwnProperty(headItem)) {
-                newItem.push({
-                  val:    item[headItem],
-                  key:    headItem,
-                  inline: this.tableHeader[i].inline // if header inline = 1
-                })
-              }
-            })
-
-            displayTable.push(newItem)
-          })
-
-          this.$store.commit('table_status_success')
-          return displayTable
-        }
-      },
-
-      // Поиск по полю label && keywords
-      filterSearch () {
-
-        if (this.table.body) {
-
-          const tableBody = clone(this.table.body)
-
-          let table = tableBody
-
-          if (this.table.settings.sort) {
-            const sortBy    = this.table.settings.sort.name,
-                  sortOrder = this.table.settings.sort.order
-
-            table = tableBody.sort(this.compareValues(sortBy, sortOrder))
-          }
-
-          return table.filter(item => {
-            return !this.searchInput
-              || item.name.toLowerCase().indexOf(this.searchInput.toLowerCase()) > -1
-              || item.label.toLowerCase().indexOf(this.searchInput.toLowerCase()) > -1
-          })
-          //this.$store.commit('table_status_success')
-        }
-      }
-
-    },
-
-    methods: {
-
-      // сохранение настроек видимости колонок таблицы
-      updateVisibleColumns (item) {
-        localStorage.setItem(
-          this.pageComponent + '_header_' + this.tableId, JSON.stringify(item)
-        )
-
-      },
-
-      async add_row () {
-        this.$store.commit('editPanel_add', true)
-        await this.$store.dispatch(this.editPanel_api.addProto, this.$route.params.id)
-        this.$store.commit('editPanel_add', true)
-      },
-
-      // отмена выделения всех строк
-      notCheckedAll () {
-        this.checked = false
-      },
-
-      // выделить все строки
-      checkedAll () {
-        this.checked = !this.checked
-      },
-
-      // Очистка поля поиска
-      clearSearchVal () {
-        this.searchInput = null
-      },
-
-      // sort Function
-      compareValues (key, order = 'asc') {
-        return function (a, b) {
-          if (!a.hasOwnProperty(key) || !b.hasOwnProperty(key)) {
-            // свойства нет ни в одном из объектов
-            return 0
-          }
-
-          const varA = (typeof a[key] === 'string') ?
-            a[key].toUpperCase() : a[key]
-          const varB = (typeof b[key] === 'string') ?
-            b[key].toUpperCase() : b[key]
-
-          let comparison = 0
-          if (varA > varB) {
-            comparison = 1
-          } else if (varA < varB) {
-            comparison = -1
-          }
-          return (
-            (order === 'desc') ? (comparison * -1) : comparison
-          )
-        }
+      editRequired: {
+        name:        true,
+        placeholder: true,
+        label:       true,
+        type:        true
       }
 
     }
+  },
+
+  watch: {
+
+    table () {
+      this.searchInput = null
+
+      //this.getHeader()
+
+      if (this.table) {
+        const headerLocal = localStorage.getItem(this.pageComponent + '_header_' + this.tableId)
+        if (headerLocal) {
+          this.header = JSON.parse(headerLocal)
+        }
+        else {
+          this.header = this.getHeader()
+          //this.header = clone(this.protoLeaf)
+
+        }
+      }
+    }
+
+  },
+
+  async created () {
+    if (this.notEmptyTable === 'error') {
+      this.$store.commit('card_right_show', false)
+      this.$store.commit('tree_active', this.tableId)
+      await this.$store.dispatch(this.table_api.get, this.tableId)
+    }
+  },
+
+  async mounted () {
+    this.searchInput = null
+    //if (this.notEmptyTable === 'error') {
+    //  this.$store.commit('card_right_show', false)
+    //  this.$store.commit('tree_active', this.tableId)
+    //  await this.$store.dispatch(this.table_api.get, this.tableId)
+    //}
+  },
+
+  beforeRouteUpdate (to, from, next) {
+    this.searchInput = null
+    next()
+  },
+
+  beforeDestroy () {
+    this.$store.commit('editPanel_show', false)
+    this.$store.commit('tree_active', null)
+    this.$store.commit('set_editPanel_proto', [])
+    this.$store.commit('set_tree_proto', [])
+  },
+
+  computed: {
+
+    pageComponent () {return this.$route.name},
+
+    editPanel_api () {return this.$store.getters.editPanel_api},
+
+    table_api () {return this.$store.getters.table_api},
+
+    tableNotEmpty () {
+      if (this.table.body) {
+        return this.table.body.length > 0
+      }
+    },
+
+    tableEmptySearchResult () {
+      if (this.searchInput) {
+        return this.filterSearch.length === 0
+      }
+    },
+
+    tableId () {return this.$route.params.id},
+
+    loader () {return this.$store.getters.table_status},
+
+    protoLeaf () {return this.$store.getters.editPanel_proto},
+
+    notEmptyTable () {return (this.table && Object.keys(this.table).length === 0) ? 'error' : 'success'},
+
+    table () {return this.$store.getters.table_items},
+
+    allowAddChildren () {return this.$store.state.table.table.addChildren},
+
+    massEdit () {
+      if (this.table && this.table.settings && this.table.settings.massEdit) {
+        return this.table.settings.massEdit
+      }
+      else {
+        return 0
+      }
+    },
+
+    removable () {
+      return (this.table && this.table.settings && this.table.settings.removable)
+        ? this.table.settings.removable
+        : 0
+    },
+
+    editable () {
+      return (this.table && this.table.settings && this.table.settings.editable)
+        ? this.table.settings.editable
+        : 0
+    },
+
+    tableRowDetail () {return this.$store.getters.pageTableRow},
+
+    // Шапка таблицы
+    tableHeader () {
+      if (this.header && this.header.length > 0) {
+        return this.header.filter(item => item.show === 1)
+      }
+    },
+
+    //async tableHeaderName () {
+    //  if (this.tableHeader && this.tableHeader.length > 0) {
+    //    return await this.tableHeader.map(item => item.name)
+    //  }
+    //},
+
+    //Тело таблицы
+    tableRows () {
+      if (this.filterSearch && this.tableHeader) {
+        const table        = this.filterSearch,
+              displayTable = [],
+              flatHeader   = this.tableHeader.map(item => item.name)
+
+        table.forEach((item) => {
+          const newItem = []
+
+          flatHeader.forEach((headItem, i) => {
+
+            if (item.hasOwnProperty(headItem)) {
+              newItem.push({
+                val:    item[headItem],
+                key:    headItem,
+                inline: this.tableHeader[i].inline // if header inline = 1
+              })
+            }
+          })
+
+          displayTable.push(newItem)
+        })
+
+        this.$store.commit('table_status_success')
+        return displayTable
+      }
+    },
+
+    tableRowsField () {
+      if (!this.table) return
+      if (!this.table.body.length) return
+      return this.table.body[0]
+    },
+
+    // Поиск по полю label && keywords
+    filterSearch () {
+
+      if (this.table.body) {
+
+        const tableBody = clone(this.table.body)
+
+        let table = tableBody
+
+        if (this.table.settings.sort) {
+          const sortBy    = this.table.settings.sort.name,
+                sortOrder = this.table.settings.sort.order
+
+          table = tableBody.sort(this.compareValues(sortBy, sortOrder))
+        }
+
+        return table.filter(item => {
+          return !this.searchInput
+            || item.name.toLowerCase().indexOf(this.searchInput.toLowerCase()) > -1
+            || item.label.toLowerCase().indexOf(this.searchInput.toLowerCase()) > -1
+        })
+        //this.$store.commit('table_status_success')
+      }
+    }
+
+  },
+
+  methods: {
+
+    getHeader () {
+      if (this.tableRowsField) {
+
+        const header    = []
+        const protoLeaf = this.protoLeaf
+        const row       = this.tableRowsField
+
+        for (let prop in row) {
+          if (row.hasOwnProperty(prop)) {
+            const index = protoLeaf.findIndex(i => i.name === prop)
+            if (index >= 0) {
+              header.push(protoLeaf[index])
+            }
+          }
+        }
+        return header
+        //return header.filter(item => item.show === 1)
+      }
+    },
+
+    // сохранение настроек видимости колонок таблицы
+    updateVisibleColumns (item) {
+      localStorage.setItem(
+        this.pageComponent + '_header_' + this.tableId, JSON.stringify(item)
+      )
+    },
+
+    async add_row () {
+      //this.$store.commit('editPanel_add', true)
+      await this.$store.dispatch(this.editPanel_api.addProto, this.$route.params.id)
+      this.$store.commit('editPanel_add', true)
+    },
+
+    // отмена выделения всех строк
+    notCheckedAll () {this.checked = false},
+
+    // выделить все строки
+    checkedAll () {this.checked = !this.checked},
+
+    // Очистка поля поиска
+    clearSearchVal () {this.searchInput = null},
+
+    // sort Function
+    compareValues (key, order = 'asc') {
+      return function (a, b) {
+        if (!a.hasOwnProperty(key) || !b.hasOwnProperty(key)) {
+          // свойства нет ни в одном из объектов
+          return 0
+        }
+
+        const varA = (typeof a[key] === 'string') ?
+          a[key].toUpperCase() : a[key]
+        const varB = (typeof b[key] === 'string') ?
+          b[key].toUpperCase() : b[key]
+
+        let comparison = 0
+        if (varA > varB) {
+          comparison = 1
+        }
+        else if (varA < varB) {
+          comparison = -1
+        }
+        return (
+          (order === 'desc') ? (comparison * -1) : comparison
+        )
+      }
+    }
+
   }
+}
 </script>
