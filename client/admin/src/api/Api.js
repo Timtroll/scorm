4267f766a83fef12b59c5c4c210a782a0619f8be
Yurgeman <Yurgeman@users.noreply.github.com>
2020-09-13 12:28:15 +0300

    Update Api.js

--- client/admin/src/api/Api.js
+++ client/admin/src/api/Api.js
@@ -1,10 +1,12 @@
-import Vue from 'vue'
+import Vue   from 'vue'
 import axios from 'axios'
 
 Vue.prototype.$http = axios
 const token         = localStorage.getItem('token')
 
-if (token) { Vue.prototype.$http.defaults.headers.common['token'] = token}
+if (token) {
+  Vue.prototype.$http.defaults.headers.common['token'] = token
+}
 //Vue.prototype.$http.defaults.headers.common['Access-Control-Allow-Origin'] = 'https://cors-c.herokuapp.com'
 //Vue.prototype.$http.defaults.headers.common['Access-Control-Allow-Origin'] = 'https://freee.su'
 //Vue.prototype.$http.defaults.headers.common['Access-Control-Allow-Origin'] = 'http://localhost:8080'
@@ -16,22 +18,20 @@ if (Vue.config.productionTip) {
   //Vue.prototype.$http.defaults.headers.common['Access-Control-Allow-Origin'] = 'http://localhost:8080'
   //Vue.prototype.$http.defaults.headers.common['Access-Control-Allow-Credentials'] = 'true'
   apiProxy = '' // для Localhost  https://cors-c.herokuapp.com
-  //apiProxy = 'https://cors-c.herokuapp.com/https://freee.su/' // для Localhost  https://cors-c.herokuapp.com
+  apiProxy = 'https://cors-c.herokuapp.com/https://freee.su/' // для Localhost  https://cors-c.herokuapp.com
 }
-axios.defaults.withCredentials = true
+axios.defaults.withCredentials = false
 //axios.defaults.withCredentials = !Vue.config.productionTip
 
-const apiUrl = apiProxy + 'https://freee.su/'
+const apiUrl = apiProxy + '/'
 
-const header = {
-
-}
 export default () => {
 
   return axios.create({
-    baseURL:     apiUrl,
-    crossDomain: true,
-    headers:     {
+    baseURL: apiUrl,
+    method:  'post',
+    //crossDomain: true,
+    headers: {
       'Accept':       'application/json',
       'Content-type': 'application/x-www-form-urlencoded',
       'Content-Type': 'application/json'
