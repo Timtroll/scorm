c120d0f2614ee3bbf5a0555368414847a1a7da58
AnosovSemen <Semeon222@yandex.ru>
2020-09-29 10:48:21 +0300

    installation update

--- script/install.pl
+++ script/install.pl
@@ -9,6 +9,7 @@
 # --path='./temp_freee.conf'
 # --mode=test - создание базы данных scorm_test при её отсутствии
 # --rebuild=1/0 - создание базы и конфигурации или только конфигурации
+# --start=test - по окончании работы скрипта, старт mojo с тестовой базой
 
 use utf8;
 use strict;
@@ -23,8 +24,9 @@ use Data::Dumper;
 use DDP;
 use Freee::Mock::Install;
 
-my ( $path_sql, $path_conf, $path_log, $text, $rebuild, $config_update, $test, $db, $self, $res, $sth, $database, $filename, $create_db, $sql, $host, $url, @list );
+my ( $path_sql, $path_conf, $path_log, $text, $rebuild, $config_update, $db, $self, $res, $sth, $database, $filename, $create_db, $sql, $host, $url, $result, @bd_array, @list );
 
+# чтение параметров
 my %hash;
 foreach my $parameter ( @ARGV ) {
     $parameter = substr( $parameter, 2 );
@@ -48,9 +50,9 @@ $path_sql = './sql';
 # путь к директории с конифгурацией
 $path_conf = './freee.conf';
 
-# проверка параметра test
-if ( $test &&  $test ne 'test' ) {
-    logging( "wrong format '$test' of 'test' parameter" ); 
+# проверка параметров test
+if ( $hash{'mode'} &&  $hash{'mode'} ne 'test' || $hash{'start'} &&  $hash{'start'} ne 'test' ) {
+    logging( "wrong format '$hash{'mode'}' of 'test' parameter" ); 
     exit;    
 }
 
@@ -67,71 +69,154 @@ else {
     exit;
 }
 
-# остановка демона
-`./starting.sh stop`;
-
 $config_update = eval( $text );
 
-# обновление конфигурации
-$$config{ 'expires' }                                           = $$config_update{ 'expires' }               if $$config_update{ 'expires' };
-$$config{ 'debug' }                                             = $$config_update{ 'debug' }                 if $$config_update{ 'debug' };
-$$config{ 'test' }                                              = $$config_update{ 'test' }                  if $$config_update{ 'test' };
-$$config{ 'export_settings_path' }                              = $$config_update{ 'export_settings_path' }  if $$config_update{ 'export_settings_path' };
-$config->{'dbs'}->{'databases'}->{'pg_main'}->{'username'}      = $$config_update{ 'pg_main_username' }      if $$config_update{ 'pg_main_username' }; 
-$config->{'dbs'}->{'databases'}->{'pg_main'}->{'password'}      = $$config_update{ 'pg_main_password' }      if $$config_update{ 'pg_main_password' };
-$config->{'dbs'}->{'databases'}->{'pg_main'}->{'options'}       = $$config_update{ 'pg_main_options' }       if $$config_update{ 'pg_main_options' };
-$config->{'dbs'}->{'databases'}->{'pg_main_test'}->{'username'} = $$config_update{ 'pg_test_username' }      if $$config_update{ 'pg_test_username' };
-$config->{'dbs'}->{'databases'}->{'pg_main_test'}->{'password'} = $$config_update{ 'pg_test_password' }      if $$config_update{ 'pg_test_password' };
-$config->{'dbs'}->{'databases'}->{'pg_main_test'}->{'options'}  = $$config_update{ 'pg_test_options' }       if $$config_update{ 'pg_test_options' };
-
-# настройка dumper, чтобы не было лишнего 'var='
-$Data::Dumper::Terse = 1;
-
-# запись конфигурации в файл
-my $result = write_file(
-    $path_conf,
-    Dumper( $config )
-);
-
+# проверка параметра rebuild
 if ( $hash{'rebuild'} ) {
-    unless ( $test ) {
-        $db = $config->{'dbs'}->{'databases'}->{'pg_main'};
-    } 
-    else {
-        $db = $config->{'dbs'}->{'databases'}->{'pg_main_test'};
-    }
-
-    # редактирование параметра обработки ошибок
-    if (
-        $db &&
-        $db->{'options'} &&
-        $db->{'options'}->{'RaiseError'} 
-    ) {
-        $db->{'options'}->{'RaiseError'} = 1;
-    }
-
-    # подключение к базе данных
+    # подключение к базе postgres
     $self->{dbh} = DBI->connect(
         'dbi:Pg:dbname=postgres;host=localhost;port=5432',
-        $db->{'username'},
-        $db->{'password'},
-        $db->{'options'}
+        'troll',
+        'Yfenbkec_1',
+        { 
+            'pg_enable_utf8' => 1, 
+            'pg_auto_escape' => 1, 
+            'AutoCommit' => 1, 
+            'PrintError' => 1, 
+            'RaiseError' => 1, 
+            'pg_server_prepare' => 0 
+        }
     );
     if ( DBI->errstr ) {
         logging( "connection to database doesn't work:" . DBI->errstr );
         exit;    
     }
 
-    # проверка существования бд
-    $database = $test ? 'scorm_test' : 'scorm';
-    if ( check_db( $database ) ) {
-        warn 'database ' . $database . ' already exists';
-        logging( 'database ' . $database . ' already exists' );
-        exit;    
+    # проверка отсутствия базы scorm
+    if ( check_db( 'scorm' ) ) {
+        logging( 'database scorm already exists' );
+        exit; 
     }
 
-    # создание базы
-    $create_db = $test ? '/_create_test_db.sql' : '/_create_db.sql';
+    # создаём две базы или одну
+    if ( $hash{'mode'} ) {
+
+        # проверка отсутствия базы scorm_test
+        if ( check_db( 'scorm_test' ) ) {
+            logging( 'database scorm_test already exists' );
+            exit; 
+        }
+
+        # массив из test, scorm
+        if ( $hash{'start'} ) {
+            @bd_array = ( 'scorm', 'scorm_test' );
+        }
+        else {
+            @bd_array = ( 'scorm_test', 'scorm' );
+        }
+    }
+    else{
+        # массив только scorm
+        @bd_array = ( 'scorm' );
+    }
+
+    # для каждого эл-та массива
+    foreach( @bd_array ) {
+
+        # остановка mojo
+        mojo_do( 'stop ');
+
+        # если это тестовая база
+        if ( $_ eq 'scorm_test' ) {
+            # заполнение параметров конфига для тестовой базы ---
+            create_db( '/_create_test_db.sql' );
+            connect_db( 'pg_main_test' );
+        }
+        else {
+            # заполнение параметров конфига для основной базы ---
+            create_db( '/_create_db.sql' );
+            connect_db( 'pg_main' );
+        }
+
+        # запись конфигурации в файл freee.conf
+        write_config();
+
+        # запуск скриптов
+        create_tables();
+
+        # старт mojo
+        mojo_do( 'start ');
+
+        # загрузка дефолтных значений
+        load_defaults();
+    }
+}
+else {
+    # заполнение параметров конфига для основной базы scorm
+    $$config{ 'expires' }                                           = $$config_update{ 'expires' }               if $$config_update{ 'expires' };
+    $$config{ 'debug' }                                             = $$config_update{ 'debug' }                 if $$config_update{ 'debug' };
+    $$config{ 'test' }                                              = $$config_update{ 'test' }                  if $$config_update{ 'test' };
+    $$config{ 'export_settings_path' }                              = $$config_update{ 'export_settings_path' }  if $$config_update{ 'export_settings_path' };
+    $config->{'dbs'}->{'databases'}->{'pg_main'}->{'username'}      = $$config_update{ 'pg_main_username' }      if $$config_update{ 'pg_main_username' }; 
+    $config->{'dbs'}->{'databases'}->{'pg_main'}->{'password'}      = $$config_update{ 'pg_main_password' }      if $$config_update{ 'pg_main_password' };
+    $config->{'dbs'}->{'databases'}->{'pg_main'}->{'options'}       = $$config_update{ 'pg_main_options' }       if $$config_update{ 'pg_main_options' };
+
+    # если тест
+    if ( $hash{'mode'} ) {
+        $config->{'dbs'}->{'databases'}->{'pg_main_test'}->{'username'} = $$config_update{ 'pg_main_test_username' } if $$config_update{ 'pg_main_test_username' };
+        $config->{'dbs'}->{'databases'}->{'pg_main_test'}->{'password'} = $$config_update{ 'pg_main_test_password' } if $$config_update{ 'pg_main_test_password' };
+        $config->{'dbs'}->{'databases'}->{'pg_main_test'}->{'options'}  = $$config_update{ 'pg_main_test_options' }  if $$config_update{ 'pg_main_test_options' };
+    }
+
+    # запись конфигурации в файл freee.conf
+    write_config();
+}
+
+
+####################################################################
+
+
+# логирование ошибки
+# logging( "комментарий и текст ошибки" );
+sub logging {
+    my $logdata = shift;
+    my $log;
+
+    if ( -e $path_log ) {
+        open( $log, '>>', $path_log ) or warn "Can't open log file!";
+            print $log "$logdata\n";
+        close( $log );
+    }
+}
+
+# проверка существования базы данных
+# my true = check_db( database );
+sub check_db {
+    my $database = shift;
+
+    my $check_db = 'SELECT datname FROM pg_database WHERE datname = ?';
+    $sth = $self->{dbh}->prepare( $check_db );
+    $sth->bind_param( 1, $database );
+    $res = $sth->execute();
+    $res = 0 if $res == '0E0';
+
+    return $res;
+}
+
+# запуск или остановка mojo
+# my true = mojo_do( do );
+sub mojo_do {
+    my $do = shift;
+
+    $res = `./starting.sh $do`;
+    return $res;
+}
+
+# my true = create_db(  );
+# создание базы данных
+sub create_db {
+    $create_db = shift;
+
     $filename = $path_sql . $create_db;
     $sql = slurp( $filename, encoding => 'utf8' );
     $sth = $self->{dbh}->prepare( $sql );
@@ -142,8 +227,35 @@ if ( $hash{'rebuild'} ) {
         exit;
     }
     warn 'Db created';
+}
 
-    # подключение к базе данных
+# заполнение параметров и создание конфига 
+# my true = connect_db( main );
+sub connect_db {
+    my $main = shift;
+
+    # обновление конфигурации
+    $$config{ 'test' }                                          = $main eq 'pg_main_test' ? 1 : 0;
+
+    $$config{ 'expires' }                                       = $$config_update{ 'expires' }               if $$config_update{ 'expires' };
+    $$config{ 'debug' }                                         = $$config_update{ 'debug' }                 if $$config_update{ 'debug' };
+    $$config{ 'export_settings_path' }                          = $$config_update{ 'export_settings_path' }  if $$config_update{ 'export_settings_path' };
+
+    $config->{'dbs'}->{'databases'}->{$main}->{'username'}      = $$config_update{ $main . '_username' }     if $$config_update{ $main . '_username' }; 
+    $config->{'dbs'}->{'databases'}->{$main}->{'password'}      = $$config_update{ $main . '_password' }     if $$config_update{ $main . '_password' };
+    $config->{'dbs'}->{'databases'}->{$main}->{'options'}       = $$config_update{ $main . '_options' }      if $$config_update{ $main . '_options' };
+
+    $db = $config->{'dbs'}->{'databases'}->{$main};
+
+    # редактирование параметра обработки ошибок
+    if (
+        $db &&
+        $db->{'options'} &&
+        $db->{'options'}->{'RaiseError'} 
+    ) {
+        $db->{'options'}->{'RaiseError'} = 1;
+    }
+
     $self->{dbh} = DBI->connect(
         $db->{'dsn'},
         $db->{'username'},
@@ -154,7 +266,9 @@ if ( $hash{'rebuild'} ) {
         logging( "connection to database doesn't work:" . DBI->errstr );
         exit;
     }
+}
 
+sub create_tables {
     # создание расширения
     $filename = $path_sql . '/_create_extiention.sql';
     $sql = slurp( $filename, encoding => 'utf8' );
@@ -196,10 +310,10 @@ if ( $hash{'rebuild'} ) {
             exit;    
         }
     }
+}
 
-    # старт демона
-    `./starting.sh start`;
-
+# загрузка дефолтных значений
+sub load_defaults {
     # загрузка дефолтных настроек
     $host = $config->{'host'};
     $url = $host . '/settings/load_default';
@@ -260,34 +374,15 @@ if ( $hash{'rebuild'} ) {
     $sth = $self->{'dbh'}->prepare( $sql );
     $result = $sth->execute();
 }
-exit;
 
+# запись конфигурации в файл freee.conf
+sub write_config {
+    # настройка dumper, чтобы не было лишнего 'var='
+    $Data::Dumper::Terse = 1;
 
-####################################################################
-
-# логирование ошибки
-# logging( "комментарий и текст ошибки" );
-sub logging {
-    my $logdata = shift;
-    my $log;
-
-    if ( -e $path_log ) {
-        open( $log, '>>', $path_log ) or warn "Can't open log file!";
-            print $log "$logdata\n";
-        close( $log );
-    }
-}
-
-# проверка существования базы данных
-# my true = check_db( database );
-sub check_db {
-    my $database = shift;
-
-    my $check_db = 'SELECT datname FROM pg_database WHERE datname = ?';
-    $sth = $self->{dbh}->prepare( $check_db );
-    $sth->bind_param( 1, $database );
-    $res = $sth->execute();
-    $res = 0 if $res == '0E0';
-
-    return $res;
+    # запись конфигурации в файл
+    my $result = write_file(
+        $path_conf,
+        Dumper( $config )
+    );
 }
